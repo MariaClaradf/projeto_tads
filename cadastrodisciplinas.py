1514799d4e41653841e60811d99e98280cdcdd69
MariaClaradf <maria.faria@estudante.ifms.edu.br>
2025-01-06 18:37:52 -0300

    cadastro disciplina feito

--- cadastrodisciplinas.py
+++ cadastrodisciplinas.py
@@ -2,17 +2,15 @@ from random import randint
 list = []
 
 
-def num_matricula():
-    num = str(randint(10000, 50000))
-    return num
+def cod_disciplina():
+    cod = str(randint(1000, 3000))
+    return cod
 
-def cadastro_aluno():
-    nome = input('Digite seu nome: ')
-    matricula = num_matricula()
-    nascimento = input('Digite sua data de nascimento: ')
-    genero = input('Digite seu genero: ')
-    endereco = input('Digite seu endereço: ')
-    telefone = input('Digite o seu telefone: ')
-    email = input('Digite seu e-mail: ')
-    list.append({'nome': nome, 'matricula': matricula, 'nascimento': nascimento, 'genero': genero, 'endereco': endereco, 'telefone': telefone, 'email': email})
-cadastro_aluno()
+def cadastro_disciplina():
+    nome = input('Digite o nome da sua disciplina: ')
+    codigo = cod_disciplina()
+    horas = input('Digite sua carga horária: ')
+    prof = input('Digite o nome do professor: ')
+    
+    list.append({'nome': nome, 'código': codigo, 'carga horária': horas, 'professor': prof})
+cadastro_disciplina()
