4eb3b1c0f1933f36160f893de869e3c089837019
MariaClaradf <maria.faria@estudante.ifms.edu.br>
2025-01-06 18:29:00 -0300

    correção cadastro prof

--- cadastroprofessores.py
+++ cadastroprofessores.py
@@ -6,7 +6,7 @@ def num_matricula():
     num = str(randint(10000, 50000))
     return num
 
-def cadastro_aluno():
+def cadastro_professores():
     nome = input('Digite seu nome: ')
     matricula = num_matricula()
     nascimento = input('Digite sua data de nascimento: ')
@@ -16,4 +16,4 @@ def cadastro_aluno():
     email = input('Digite seu e-mail: ')
     disciplina = input('Digite a sua disciplina: ')
     list.append({'nome': nome, 'matricula': matricula, 'nascimento': nascimento, 'genero': genero, 'endereco': endereco, 'telefone': telefone, 'email': email, 'disciplna': disciplina})
-cadastro_aluno()
+cadastro_professores()
